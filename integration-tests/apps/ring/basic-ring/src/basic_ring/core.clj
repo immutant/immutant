cf124505059de8d82a429cffa01068cd4baf7f4e
Jim Crossley <jim@crossleys.org>
2011-10-24 16:03:19 -0400

    Kinda noisy

--- integration-tests/apps/ring/basic-ring/src/basic_ring/core.clj
+++ integration-tests/apps/ring/basic-ring/src/basic_ring/core.clj
@@ -5,8 +5,7 @@
 
 (defn handler [request]
   (let [body (str "Hello From Clojure inside TorqueBox! This is basic-ring<pre>" shared? "</pre><img src='biscuit.jpg'/>")
-        services (seq (map #(.getCanonicalName %) (.getServiceNames service/registry)))
         factory (service/service "jboss.naming.context.java.ConnectionFactory")]
     {:status 200
      :headers {"Content-Type" "text/html"}
-     :body (str body "<pre>" services "</pre><p>" factory "</p>")}))
+     :body (str body "<p>" factory "</p>")}))
