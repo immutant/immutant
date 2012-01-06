4b0a4c7ba2031730b3f6a65ff066d3d01134dd12
Jim Crossley <jim@crossleys.org>
2012-01-06 18:06:48 -0500

    Get emacs --batch working

--- docs/bin/publish.el
+++ docs/bin/publish.el
@@ -1,25 +1,30 @@
+;; emacs --batch --load bin/publish.el --visit src/org/index.org --funcall org-publish-current-project
+
 (require 'org-publish)
 
-(setq org-publish-project-alist
-      `(
-        ("org"
-         :base-directory ,(expand-file-name "../src/org/")
-         :base-extension "org"
-         :publishing-directory ,(expand-file-name "../target/")
-         :recursive t
-         :publishing-function org-publish-org-to-html
-         :headline-levels 4             ; Just the default for this project.
-         :author-info nil
-         :email-info nil
-         :creator-info nil
-         :html-postamble nil
-         )
-        ("static"
-         :base-directory ,(expand-file-name "../src/org/")
-         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-         :publishing-directory ,(expand-file-name "../target/")
-         :recursive t
-         :publishing-function org-publish-attachment
-         )
-        ("docs" :components ("org" "static"))
-        ))
+(let ((dir (if (buffer-file-name) (expand-file-name "../" (file-name-directory (buffer-file-name))) default-directory)))
+  (setq org-publish-use-timestamps-flag nil
+        org-publish-project-alist
+        `(
+          ("org"
+           :base-directory ,(expand-file-name "src/org/" dir)
+           :base-extension "org"
+           :publishing-directory ,(expand-file-name "target/" dir)
+           :recursive t
+           :publishing-function org-publish-org-to-html
+           :headline-levels 2 
+           :author-info nil
+           :email-info nil
+           :creator-info nil
+           :html-postamble nil
+           )
+          ("static"
+           :base-directory ,(expand-file-name "src/org/" dir)
+           :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+           :publishing-directory ,(expand-file-name "target/" dir)
+           :recursive t
+           :publishing-function org-publish-attachment
+           )
+          ("docs" :components ("org" "static"))
+          )))
+
