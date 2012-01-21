50c822d6625a70261f993d9ff2556e8e58f168cd
Tobias Crawley <tcrawley@gmail.com>
2012-01-20 21:35:46 -0500

    Post process for THE GREATER GOOD.

--- docs/bin/publish.el
+++ docs/bin/publish.el
@@ -1,15 +1,12 @@
 ;; emacs --batch --load bin/publish.el --visit src/org/index.org --funcall org-publish-current-project
 
-(defvar immutant-version "${project.version}")
-
-(defun create-postamble ()
-  (concat "<div class=\"version\">" immutant-version "</div>"))
-
-
-(let ((dir (if (buffer-file-name) (expand-file-name "../" (file-name-directory (buffer-file-name))) default-directory)))
-  (setq load-path (cons (expand-file-name "elisp/" dir) load-path))
-  (setq load-path (cons (expand-file-name "elisp/org-mode/lisp" dir) load-path))
-  (setq load-path (cons (expand-file-name "elisp/org-mode/contrib/lisp" dir) load-path))
+(let* ((dir (if (buffer-file-name)
+               (expand-file-name "../" (file-name-directory (buffer-file-name)))
+             default-directory))
+      (nav "<ul class=\"docnav\"><li class=\"previous\"><a accesskey=\"p\" href=\"\"><strong>Prev</strong></a></li><li class=\"home\"><a accesskey=\"h\" href=\"index.html\"><strong>Table Of Contents</strong></a></li><li class=\"next\"><a accesskey=\"n\" href=\"\"><strong>Next</strong></a></li></ul>"))
+  (setq load-path (cons (expand-file-name "elisp/" dir) load-path)
+        load-path (cons (expand-file-name "elisp/org-mode/lisp" dir) load-path)
+        load-path (cons (expand-file-name "elisp/org-mode/contrib/lisp" dir) load-path))
 
   (require 'clojure-mode)
   (require 'org-publish)
@@ -24,15 +21,15 @@
           ("org"
            :base-directory ,(expand-file-name "src/org/" dir)
            :base-extension "org"
-           :publishing-directory ,(expand-file-name "target/html/" dir)
+           :publishing-directory ,(expand-file-name "target/tmp/" dir)
            :recursive t
            :publishing-function org-publish-org-to-html
            :headline-levels 2
            :author-info nil
            :email-info nil
            :creator-info nil
-           :html-preamble "<p id=\"title\"><span class=\"site\"><strong>JBoss.org</strong></span><span class=\"doc\"><strong>Community Documentation</strong></span></p>"
-           :html-postamble create-postamble
+           :html-preamble ,(concat "<p id=\"title\"><span class=\"site\"><strong>JBoss.org</strong></span><span class=\"doc\"><strong>Community Documentation</strong></span></p>" nav)
+           :html-postamble ,(concat nav "<div class=\"version\">Immutant ${project.version}</div>")
            :style "<link rel='stylesheet' type='text/css' href='css/stylesheet.css' />"
            )
           ("static"
