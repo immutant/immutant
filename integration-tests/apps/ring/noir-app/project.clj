7fd1258a5ee1bb8d799a3b7ed3e44dcc85f5a304
Jim Crossley <jim@crossleys.org>
2012-02-07 14:52:06 -0500

    Upgrade Noir to use the Hiccup middleware to properly deal with a context path.
    Style still messed up due to static path in css from jar, however.

--- integration-tests/apps/ring/noir-app/project.clj
+++ integration-tests/apps/ring/noir-app/project.clj
@@ -1,6 +1,6 @@
 (defproject noir-app "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.3.0-alpha5"]]
             :main noir-app.server)
 
