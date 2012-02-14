57ccdce0e7d05bfa3de2825bd4d8b010baa621f0
Tobias Crawley <tcrawley@gmail.com>
2012-02-13 20:51:25 -0500

    Maybe this will make things work for the Jimbotron.

--- docs/bin/prep-elisp.rb
+++ docs/bin/prep-elisp.rb
@@ -6,10 +6,9 @@ CLOJURE_MODE = "https://raw.github.com/jochu/clojure-mode/master/clojure-mode.el
 
 elisp_dir = File.join( File.dirname( __FILE__ ), '..', 'target', 'elisp' )
 if !File.exists?( elisp_dir )
-  puts elisp_dir
   FileUtils.mkdir_p( elisp_dir )
   Dir.chdir( elisp_dir ) do
-    `wget #{CLOJURE_MODE}`
+    `wget --no-check-certificate #{CLOJURE_MODE}`
     `wget #{ORG_MODE}`
     `unzip #{ORG_VERSION}.zip`
     FileUtils.mv( ORG_VERSION, 'org-mode' )
