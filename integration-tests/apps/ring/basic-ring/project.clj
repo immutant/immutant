56c80b74ffd342c0118f59c3c968ef0be604c123
Tobias Crawley <tcrawley@gmail.com>
2012-02-28 08:31:50 -0500

    Don't ignore these project files.

---
+++ integration-tests/apps/ring/basic-ring/project.clj
@@ -0,0 +1,4 @@
+(defproject
+  basic-ring "1.0.0-SNAPSHOT" 
+  :description "FIXME: write description" 
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ integration-tests/apps/ring/sessions/project.clj
@@ -0,0 +1,3 @@
+(defproject sessions "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
