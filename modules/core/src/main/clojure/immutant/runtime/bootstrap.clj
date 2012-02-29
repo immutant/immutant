78aad20c5f27934de2fa2772dd49763eadd010a9
Tobias Crawley <tcrawley@gmail.com>
2012-02-29 12:02:41 -0500

    Track leiningen-core with our own published snapshot.

--- modules/core/src/main/clojure/immutant/runtime/bootstrap.clj
+++ modules/core/src/main/clojure/immutant/runtime/bootstrap.clj
@@ -20,9 +20,15 @@
   (:require [clojure.java.io          :as io]
             [clojure.walk             :as walk]
             [leiningen.core.classpath :as classpath]
-            [leiningen.core.project   :as project])
+            [leiningen.core.project   :as project]
+            [cemerick.pomegranate     :as pomegranate])
   (:import [java.io   FilenameFilter]
            [java.util ArrayList]))
+
+(extend-type org.jboss.modules.ModuleClassLoader
+    pomegranate/URLClasspath
+    (can-modify? [this] false)
+    (add-url [this _]))
 
 (defn ^{:private true} stringify-symbol
   "Turns a symbol into a namspace/name string."
@@ -60,7 +66,8 @@
 (defn ^{:private true} resolve-dependencies
   "Resolves dependencies from the lein project. It currently just delegates to leiningen-core."
   [project]
-  (classpath/resolve-dependencies project))
+  (when project
+    (classpath/resolve-dependencies :dependencies project)))
 
 (defn ^{:internal true} lib-dir
   "Resolve the library dir for the application."
@@ -72,13 +79,19 @@
   "Resolves the resource paths (in the AS7 usage of the term) for a leiningen application. Handles
 lein1/lein2 differences for project keys that changed from strings to vectors."
   [project]
-  (reduce (fn [acc key]
-            (let [path (project key)]
-              (concat acc (if (coll? path)
-                            path
-                            (vector path)))))
-          []
-          [:resources-path :source-path :native-path]))
+  (remove nil?
+          (reduce (fn [acc key]
+                    (let [path (project key)]
+                      (concat acc (if (coll? path)
+                                    path
+                                         (vector path)))))
+                  []
+                  [:resources-path ;; lein1
+                   :resource-paths ;; lein2
+                   :source-path    ;; lein1
+                   :source-paths   ;; lein2
+                   :native-path    ;; lein2
+                   ])))
 
 (defn ^{:private true} resource-paths-for-projectless-app
   "Resolves the resource paths (in the AS7 usage of the term) for a non-leiningen application."
