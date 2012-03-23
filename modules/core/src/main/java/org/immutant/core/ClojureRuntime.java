f911f0224858ccee78d13d2ab2c10abc2db2aea3
Tobias Crawley <tcrawley@gmail.com>
2012-03-23 14:15:55 -0400

    Remove unused imports.

--- modules/core/src/main/java/org/immutant/core/ClojureRuntime.java
+++ modules/core/src/main/java/org/immutant/core/ClojureRuntime.java
@@ -22,8 +22,6 @@ package org.immutant.core;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
