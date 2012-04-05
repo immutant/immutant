292db80d27c4c1427978315f996cb7a53f495d0b
Tobias Crawley <tcrawley@gmail.com>
2012-04-05 12:44:50 -0400

    Bundle clojure-mode and org-mode again so we're less dependent on the network to build.

--- docs/bin/prep-elisp.rb
+++ docs/bin/prep-elisp.rb
@@ -1,16 +1,15 @@
 require 'fileutils'
 
 ORG_VERSION = "org-7.8.03"
-ORG_MODE = "http://orgmode.org/#{ORG_VERSION}.zip"
-CLOJURE_MODE = "https://raw.github.com/jochu/clojure-mode/master/clojure-mode.el"
 
 elisp_dir = File.join( File.dirname( __FILE__ ), '..', 'target', 'elisp' )
+support_dir = File.join( File.dirname( __FILE__ ), '..', 'support' )
+
 if !File.exists?( elisp_dir )
   FileUtils.mkdir_p( elisp_dir )
   Dir.chdir( elisp_dir ) do
-    `wget --no-check-certificate #{CLOJURE_MODE}`
-    `wget #{ORG_MODE}`
-    `unzip #{ORG_VERSION}.zip`
+    FileUtils.cp( File.join( support_dir, 'clojure-mode.el' ), elisp_dir )
+    `unzip #{File.join( support_dir, 'org-mode.zip' )}`
     FileUtils.mv( ORG_VERSION, 'org-mode' )
   end
 end
