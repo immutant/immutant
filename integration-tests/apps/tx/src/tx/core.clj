31bd0c8d2f28d9c0cd6420e6e37a451028e9eaf7
Jim Crossley <jim@crossleys.org>
2012-06-07 15:54:50 -0400

    Spruce up a bit

--- integration-tests/apps/tx/src/tx/core.clj
+++ integration-tests/apps/tx/src/tx/core.clj
@@ -1,65 +1,60 @@
 (ns tx.core
   (:use clojure.test)
-  (:require [immutant.xa :as xa]
-            [immutant.cache :as c]
-            [immutant.messaging :as msg]
+  (:require [immutant.xa :as ixa]
+            [immutant.cache :as ic]
+            [immutant.messaging :as imsg]
             [clojure.java.jdbc :as sql]))
 
-;;; Messages sent to 'in' will be reversed and sent to 'out'
-(msg/start "/queue/in")
-(msg/start "/queue/out")
-(msg/listen "/queue/in" #(msg/publish "/queue/out" (apply str (reverse %))))
+;;; Create a JMS queue
+(imsg/start "/queue/test")
 
-;;; Infinispan cache
-(def cache (c/cache "stuff"))
+;;; And an Infinispan cache
+(def cache (ic/cache "test"))
 
-;;; In-memory, transactional database
-(defonce ds (xa/datasource "foo" {:adapter "h2" :database "mem:foo"}))
+;;; And an in-memory, transactional database
+(defonce ds (ixa/datasource "foo" {:adapter "h2" :database "mem:foo"}))
 (def spec {:datasource ds})
 
-;;; Ensure each test starts with an empty table
+;;; Helper methods to verify database activity
+(defn write-thing-to-db [name]
+  (sql/with-connection spec
+    (sql/insert-records :things {:name name})))
+(defn read-thing-from-db [name]
+  (sql/with-connection spec
+    (sql/with-query-results rows ["select name from things where name = ?" name]
+      (first rows))))
+(defn count-things-in-db []
+  (sql/with-connection spec
+    (sql/with-query-results rows ["select count(*) c from things"]
+      ((first rows) :c))))
+
+;;; Ensure each test starts with an empty table called THINGS
 (use-fixtures :each (fn [f]
-                      (c/delete-all cache)
+                      (ic/delete-all cache)
                       (sql/with-connection spec
                         (try (sql/drop-table :things) (catch Exception _))
                         (sql/create-table :things [:id :serial] [:name :varchar]))
                       (f)))
 
-;;; Helper methods to verify database activity
-(defn write-thing [name]
-  (sql/with-connection spec
-    (sql/insert-records :things {:name name})))
-(defn read-thing [name]
-  (sql/with-connection spec
-    (sql/with-query-results rows ["select name from things where name = ?" name]
-      (first rows))))
-(defn count-things []
-  (sql/with-connection spec
-    (sql/with-query-results rows ["select count(*) c from things"]
-      ((first rows) :c))))
-
-
-;;; Test definitions
-
-(deftest db+msg+cache-should-commit
-  (xa/transaction
-   (write-thing "kiwi")
-   (msg/publish "/queue/in" "kiwi")
-   (c/put cache :a 1))
-  (is (= "kiwi" (:name (read-thing "kiwi"))))
-  (is (= 1 (count-things)))
+(deftest db+msg+cache-should-commit "Test happy-path XA transaction involving three resources"
+  (ixa/transaction
+   (write-thing-to-db "kiwi")
+   (imsg/publish "/queue/test" "kiwi")
+   (ic/put cache :a 1))
+  (is (= "kiwi" (:name (read-thing-from-db "kiwi"))))
+  (is (= 1 (count-things-in-db)))
   (is (= 1 (:a cache)))
-  (is (= "iwik" (msg/receive "/queue/out"))))
+  (is (= "kiwi" (imsg/receive "/queue/test"))))
 
-(deftest db+msg+cache-should-rollback
+(deftest db+msg+cache-should-rollback "Test that all three resources rollback when exception is tossed"
   (try
-    (xa/transaction
-     (write-thing "kiwi")
-     (msg/publish "/queue/in" "kiwi")
-     (c/put cache :a 1)
-     (throw (Exception.)))
+    (ixa/transaction
+     (write-thing-to-db "kiwi")
+     (imsg/publish "/queue/test" "kiwi")
+     (ic/put cache :a 1)
+     (throw (Exception. "Rollback everything")))
     (catch Exception _))
-  (is (nil? (read-thing "kiwi")))
-  (is (= 0 (count-things)))
+  (is (nil? (read-thing-from-db "kiwi")))
+  (is (= 0 (count-things-in-db)))
   (is (nil? (:a cache)))
-  (is (nil? (msg/receive "/queue/out" :timeout 2000))))
+  (is (nil? (imsg/receive "/queue/test" :timeout 2000))))
