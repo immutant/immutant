817fcb4de7adaccb287fdfceffd700af6a0624ad
Tobias Crawley <tcrawley@gmail.com>
2012-06-20 14:08:02 -0400

    Minor nit.

--- support/build-support/src/main/clojure/immutant/build/install_module.clj
+++ support/build-support/src/main/clojure/immutant/build/install_module.clj
@@ -22,6 +22,6 @@
 
 (defn -main [assembly-path module-path]
   (let [module-dir (.getCanonicalFile (io/file module-path))]
-    (println "Installing module from " module-dir)
+    (println "Installing module from" (.getAbsolutePath module-dir))
     (init (io/file assembly-path))
     (install-module module-dir)))
