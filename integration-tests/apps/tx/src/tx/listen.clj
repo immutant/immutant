e377c34aa0ca5903a7c17ba2054b93c70e1ba91f
Jim Crossley <jim@crossleys.org>
2012-06-25 15:06:30 -0400

    Adding a set-rollback-only test that I *thought* would trigger re-delivery

--- integration-tests/apps/tx/src/tx/listen.clj
+++ integration-tests/apps/tx/src/tx/listen.clj
@@ -2,6 +2,7 @@
   (:use clojure.test)
   (:require [immutant.cache :as ic]
             [immutant.messaging :as imsg]
+            [immutant.xa.transaction :as tx]
             [tx.core :as core]))
 
 (imsg/start "/queue/trigger")
@@ -10,24 +11,31 @@
   (imsg/publish "/queue/test" "kiwi")
   (imsg/publish "/queue/remote-test" "starfruit" :host "localhost" :port 5445)
   (ic/put core/cache :a 1)
-  (if (:throw? m) (throw (Exception. "rollback"))))
+  (if (:throw? m) (throw (Exception. "rollback")))
+  (if (:rollback? m) (tx/set-rollback-only)))
 
 (imsg/listen "/queue/trigger" listener)
 
 (defn trigger-listener
-  [throw?]
-  (imsg/publish "/queue/trigger" {:throw? throw?}))
+  [& {:as opts}]
+  (imsg/publish "/queue/trigger" opts))
 
 (use-fixtures :each core/cache-fixture)
 
 (deftest transactional-writes-in-listener-should-work
-  (trigger-listener false)
+  (trigger-listener)
   (is (= "kiwi" (imsg/receive "/queue/test" :timeout 2000)))
   (is (= "starfruit" (imsg/receive "/queue/remote-test")))
   (is (= 1 (:a core/cache))))
 
+(deftest transactional-writes-in-listener-should-fail-on-exception
+  (trigger-listener :throw? true)
+  (is (nil? (imsg/receive "/queue/test" :timeout 2000)))
+  (is (nil? (imsg/receive "/queue/remote-test" :timeout 2000)))
+  (is (nil? (:a core/cache))))
+
 (deftest transactional-writes-in-listener-should-fail-on-rollback
-  (trigger-listener true)
+  (trigger-listener :rollback? true)
   (is (nil? (imsg/receive "/queue/test" :timeout 2000)))
   (is (nil? (imsg/receive "/queue/remote-test" :timeout 2000)))
   (is (nil? (:a core/cache))))
