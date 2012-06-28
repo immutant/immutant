ee7ac08526a5174c7b2f43bd2dd5c3799e2cf9f0
Jim Crossley <jim@crossleys.org>
2012-06-28 17:11:59 -0400

    Bump up noir version

--- integration-tests/apps/ring/noir-app/project.clj
+++ integration-tests/apps/ring/noir-app/project.clj
@@ -1,6 +1,6 @@
 (defproject noir-app "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.3.0-alpha5"]]
+                           [noir "1.3.0-beta10"]]
             :main noir-app.server)
 
