e41ed7b27866931a6a8101c15755a62d8dbf75cf
Jim Crossley <jim@crossleys.org>
2012-07-04 18:28:53 -0400

    Unfortunately, korma & lobos depend on incompatible java.jdbc versions
    Regardless, this serves as an example of using lobos migrations

--- integration-tests/apps/ring/noir-app/immutant.clj
+++ integration-tests/apps/ring/noir-app/immutant.clj
@@ -1,7 +1,8 @@
 (ns noir-app.init
   (:require [immutant.web :as web]
             [immutant.utilities :as util]
-            [noir.server :as server]))
+            [noir.server :as server]
+            [lobos.config]))
 
 (server/load-views (util/app-relative "src/noir_app/views"))
 (web/start "/" (server/gen-handler {:mode :dev :ns 'noir-app}))
--- integration-tests/apps/ring/noir-app/project.clj
+++ integration-tests/apps/ring/noir-app/project.clj
@@ -1,6 +1,8 @@
 (defproject noir-app "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.3.0-beta10"]]
-            :main noir-app.server)
+                           [noir "1.3.0-beta10"]
+                           [lobos "1.0.0-SNAPSHOT"]
+                           [com.h2database/h2 "1.3.160"]]
+              :immutant {:swank-port 4111})
 
