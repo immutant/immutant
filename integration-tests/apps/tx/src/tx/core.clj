bed4ba22dfe567886a88b31639aab392efbf4817
Jim Crossley <jim@crossleys.org>
2012-08-18 22:34:38 -0400

    Decided against returning a future; let the user decide [IMMUTANT-92]

--- integration-tests/apps/tx/src/tx/core.clj
+++ integration-tests/apps/tx/src/tx/core.clj
@@ -13,30 +13,30 @@
 (def cache (ic/cache "test"))
 
 ;;; And some transactional databases
-(defonce h2 (ixa/datasource "h2" {:adapter "h2" :database "mem:foo"}))
+(defonce h2 (future (ixa/datasource "h2" {:adapter "h2" :database "mem:foo"})))
 ;;; rds-create-db-instance oracle -s 10 -c db.m1.small -e oracle-se -u myuser -p mypassword --db-name mydb
-(defonce oracle (ixa/datasource "oracle" {:adapter "oracle"
-                                          :host "oracle.cpct4icp7nye.us-east-1.rds.amazonaws.com"
-                                          :username "myuser"
-                                          :password "mypassword"
-                                          :database "mydb"}))
+(defonce oracle (future (ixa/datasource "oracle" {:adapter "oracle"
+                                                  :host "oracle.cpct4icp7nye.us-east-1.rds.amazonaws.com"
+                                                  :username "myuser"
+                                                  :password "mypassword"
+                                                  :database "mydb"})))
 ;;; rds-create-db-instance mysql -s 10 -c db.m1.small -e mysql -u myuser -p mypassword --db-name mydb
-(defonce mysql (ixa/datasource "mysql" {:adapter "mysql"
-                                        :host "mysql.cpct4icp7nye.us-east-1.rds.amazonaws.com"
-                                        :username "myuser"
-                                        :password "mypassword"
-                                        :database "mydb"}))
+(defonce mysql (future (ixa/datasource "mysql" {:adapter "mysql"
+                                                :host "mysql.cpct4icp7nye.us-east-1.rds.amazonaws.com"
+                                                :username "myuser"
+                                                :password "mypassword"
+                                                :database "mydb"})))
 ;;; configured locally
-(defonce postgres (ixa/datasource "postgres" {:adapter "postgresql"
-                                              :username "myuser"
-                                              :password "mypassword"
-                                              :database "mydb"}))
+(defonce postgres (future (ixa/datasource "postgres" {:adapter "postgresql"
+                                                      :username "myuser"
+                                                      :password "mypassword"
+                                                      :database "mydb"})))
 ;;; nfi since --db-name isn't supported for RDS sqlserver-se instances
-(defonce mssql (ixa/datasource "mssql" {:adapter "mssql"
-                                        :host "mssql.cpct4icp7nye.us-east-1.rds.amazonaws.com"
-                                        :username "myuser"
-                                        :password "mypassword"
-                                        :database "mydb"}))
+(defonce mssql (future (ixa/datasource "mssql" {:adapter "mssql"
+                                                :host "mssql.cpct4icp7nye.us-east-1.rds.amazonaws.com"
+                                                :username "myuser"
+                                                :password "mypassword"
+                                                :database "mydb"})))
 
 ;;; Helper methods to verify database activity
 (defn write-thing-to-db [spec name]
@@ -54,7 +54,7 @@
 (defn attempt-transaction [ds & [f]]
   (try
     (ixa/transaction
-     (write-thing-to-db {:datasource @ds} "kiwi")
+     (write-thing-to-db {:datasource ds} "kiwi")
      (imsg/publish "/queue/test" "kiwi")
      (imsg/publish "/queue/remote-test" "starfruit" :host "localhost" :port 5445)
      (ic/put cache :a 1)
@@ -64,19 +64,19 @@
 (defn verify-transaction-success [ds]
   (is (= "kiwi" (imsg/receive "/queue/test" :timeout 2000)))
   (is (= "starfruit" (imsg/receive "/queue/remote-test")))
-  (is (= "kiwi" (:name (read-thing-from-db {:datasource @ds} "kiwi"))))
-  (is (= 1 (count-things-in-db {:datasource @ds})))
+  (is (= "kiwi" (:name (read-thing-from-db {:datasource ds} "kiwi"))))
+  (is (= 1 (count-things-in-db {:datasource ds})))
   (is (= 1 (:a cache))))
 
 (defn verify-transaction-failure [ds]
   (is (nil? (imsg/receive "/queue/test" :timeout 2000)))
   (is (nil? (imsg/receive "/queue/remote-test" :timeout 2000)))
-  (is (nil? (read-thing-from-db {:datasource @ds} "kiwi")))
-  (is (= 0 (count-things-in-db {:datasource @ds})))
+  (is (nil? (read-thing-from-db {:datasource ds} "kiwi")))
+  (is (= 0 (count-things-in-db {:datasource ds})))
   (is (nil? (:a cache))))
 
 (defn define-tests [db]
-  (eval `(let [ds# (var-get (resolve (symbol ~db)))]
+  (eval `(let [ds# @(var-get (resolve (symbol ~db)))]
            (deftest ~(symbol (str "commit-" db)) 
              (attempt-transaction ds#)
              (verify-transaction-success ds#))
@@ -84,10 +84,10 @@
              (attempt-transaction ds# #(throw (Exception. "rollback")))
              (verify-transaction-failure ds#)))))
 
-(defn db-fixture [ds]
+(defn db-fixture [db]
   (fn [f]
     (try
-      (sql/with-connection {:datasource @ds}
+      (sql/with-connection {:datasource db}
         (try (sql/drop-table :things) (catch Exception _))
         (sql/create-table :things [:name "varchar(50)"]))
       (catch Exception _))
@@ -100,7 +100,7 @@
 (defn testes [& dbs]
   (binding [*ns* *ns*]
     (in-ns 'tx.core)
-    (apply use-fixtures :each cache-fixture (map #(db-fixture (var-get (resolve (symbol %)))) dbs))
+    (apply use-fixtures :each cache-fixture (map #(db-fixture @(var-get (resolve (symbol %)))) dbs))
     (doseq [db dbs]
       (define-tests db))))
 
