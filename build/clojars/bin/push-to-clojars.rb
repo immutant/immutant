6d702778635d095b620ffbbe2c9bd84335c816f7
Tobias Crawley <tcrawley@gmail.com>
2012-09-11 09:16:57 -0400

    Add script to facilitate pushing to clojars going forward.

---
+++ build/clojars/bin/push-to-clojars.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+Dir["target/*"].each do |dir|
+  if File.directory?(dir) 
+    puts "Processing #{dir}..."
+    Dir.chdir(dir) do
+      if File.exists?("project.clj")
+        puts "-> generating pom..."
+        %x{lein pom}
+        cmd = "scp pom.xml *.jar clojars@clojars.org:"
+        puts "-> #{cmd}"
+        %x{#{cmd}}
+      else
+          puts "-> No project.clj found in #{dir} - skipping"
+      end
+    end
+  end
+end
+
+
+
