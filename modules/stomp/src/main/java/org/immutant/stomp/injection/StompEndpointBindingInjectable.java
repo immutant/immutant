3f29a4fac9d08ec5152304238c1ad06b93e10b32
Tobias Crawley <tcrawley@gmail.com>
2012-10-15 16:47:32 -0400

    Initial import of stomp module from torquebox.

---
+++ modules/stomp/src/main/java/org/immutant/stomp/injection/StompEndpointBindingInjectable.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008-2012 Red Hat, Inc, and individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.immutant.stomp.injection;
+
+import org.immutant.stomp.RubyStompletMetaData;
+import org.immutant.stomp.as.StompServices;
+import org.jboss.as.server.deployment.AttachmentList;
+import org.jboss.as.server.deployment.DeploymentUnit;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceTarget;
+import org.torquebox.core.injection.SimpleNamedInjectable;
+
+public class StompEndpointBindingInjectable extends SimpleNamedInjectable {
+    
+    public static final StompEndpointBindingInjectable INSTANCE = new StompEndpointBindingInjectable();
+
+    public StompEndpointBindingInjectable() {
+        super( "stomp-endpoint", "stomp-endpoint", false, true );
+    }
+
+    @Override
+    public ServiceName getServiceName(ServiceTarget serviceTarget, DeploymentUnit unit) throws Exception {
+        AttachmentList<RubyStompletMetaData> stomplets = unit.getAttachment( RubyStompletMetaData.ATTACHMENTS_KEY );
+        
+        if ( stomplets == null || stomplets.isEmpty() ) {
+            return null;
+        }
+        
+        return StompServices.endpointBinding( unit );
+    }
+
+}
