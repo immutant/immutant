e6f1e349fdc09543274e48d6966730130a756a11
Toby Crawley <toby@tcrawley.org>
2012-12-10 13:39:25 -0500

    Add test for app root jar mounts, and give the dir a better name [IMMUTANT-190]

--- integration-tests/apps/ring/basic-ring/project.clj
+++ integration-tests/apps/ring/basic-ring/project.clj
@@ -1,7 +1,9 @@
 (defproject
   basic-ring "1.0.0-SNAPSHOT" 
   :description "FIXME: write description" 
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 ;; this will trigger a TLD scanner error if the jar mounts aren't under the app root (IMMUTANT-190)
+                 [org.mortbay.jetty/jsp-2.1 "6.1.14"]]
   :java-source-paths ["src/java/"]
   :compile-path "classes"
   :immutant {:ham :biscuit})
--- modules/core/src/main/java/org/immutant/core/TmpResourceMounter.java
+++ modules/core/src/main/java/org/immutant/core/TmpResourceMounter.java
@@ -88,7 +88,8 @@ public class TmpResourceMounter implements Service<TmpResourceMounter> {
     }
     
     private File tmpMountDir() throws IOException {
-        File dir = new File( this.deploymentUnit.getAttachment( ApplicationMetaData.ATTACHMENT_KEY ).getRoot(), ".tmp_jar_mounts" );
+        File dir = new File( this.deploymentUnit.getAttachment( ApplicationMetaData.ATTACHMENT_KEY ).getRoot(), 
+                             ".immutant_tmp_jar_mounts" );
         dir.mkdir();
         return dir;
     }
