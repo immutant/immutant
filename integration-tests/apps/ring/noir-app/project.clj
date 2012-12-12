e1a30d93f1839f5c9caf4300430b1b1d7456757e
Toby Crawley <toby@tcrawley.org>
2012-12-12 16:25:54 -0500

    Change test to use latest noir & bultitude & noir's load-views-ns [IMMUTANT-115]

--- integration-tests/apps/ring/noir-app/project.clj
+++ integration-tests/apps/ring/noir-app/project.clj
@@ -1,7 +1,7 @@
 (defproject noir-app "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.3.0-beta10"]
+                           [noir "1.3.0"]
                            [lobos "1.0.0-SNAPSHOT"]
                            [korma "0.3.0-beta10"]
                            [com.h2database/h2 "1.3.160"]]
--- integration-tests/apps/ring/noir-app/src/immutant/init.clj
+++ integration-tests/apps/ring/noir-app/src/immutant/init.clj
@@ -4,9 +4,7 @@
             [noir.server :as server]
             [lobos.config]))
 
-;; Use this once the new bultitude and noir 1.3.0 are released
-;(server/load-views-ns 'noir-app.views)
-(server/load-views (util/app-relative "src/noir_app/views"))
+(server/load-views-ns 'noir-app.views)
 (web/start "/" (server/gen-handler {:mode :dev :ns 'noir-app}))
 
  
