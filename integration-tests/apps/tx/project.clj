b37d997dcb5c2500adfa37bc683e8867fb941c9e
Jim Crossley <jim@crossleys.org>
2013-01-04 19:06:10 -0500

    Confirmed tests work with current MySQL driver

--- integration-tests/apps/tx/project.clj
+++ integration-tests/apps/tx/project.clj
@@ -5,7 +5,7 @@
                  [lobos "1.0.0-SNAPSHOT"]
                  [com.h2database/h2 "1.3.160"]
                  [org.clojars.gukjoon/ojdbc "1.4"]
-                 [org.clojars.kjw/mysql-connector "5.1.11"]
+                 [mysql/mysql-connector-java "5.1.22"]
                  [postgresql "9.0-801.jdbc4"]
                  [net.sourceforge.jtds/jtds "1.2.4"]]
   :immutant {:swank-port 4111})
