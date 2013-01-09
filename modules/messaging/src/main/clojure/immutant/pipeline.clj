e7771c789031d4b25e5612312608dd77b3f6c5df
Toby Crawley <toby@tcrawley.org>
2013-01-09 12:27:06 -0500

    First cut at documenting pipelines.

--- modules/messaging/src/main/clojure/immutant/pipeline.clj
+++ modules/messaging/src/main/clojure/immutant/pipeline.clj
@@ -16,6 +16,54 @@
 ;; 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
 (ns immutant.pipeline
+  "Provides functions for creating and managing pipelines. A pipeline
+   is a composition of functions (\"steps\"), where each function is
+   passed the result of the previous function. It is built on top of
+   Immutant's messaging subsystem, allowing each step to have multiple
+   processing threads, and to be automatically load balanced across a
+   cluster.
+
+   The `pipeline` function takes a unique (within the scope of the
+   application) name, one or more single-arity functions, and optional
+   kwarg options, returning a function that places its argument onto
+   the pipeline when called.
+
+   Each function can be optionally be wrapped with metadata that
+   provides options for how that particular function is handled (see
+   the 'step' fn below).
+
+   Example:
+
+   (require '[immutant.pipeline :as pl])
+ 
+   (defn calculate-foo [m]
+     ...
+     (assoc m :foo value))
+ 
+   (defn save-data [m]
+     ...)
+ 
+   ;; create a pipeline
+   (defonce foo-pipeline
+     (pl/pipeline \"foo\" ;; pipelines must be named
+       (pl/step calculate-foo :concurrency 5) ;; run this step with 5 threads
+       (pl/step #(update-in % [:bar] + (:foo %)) :name :update-bar) ;; give this step a name
+       save-data ;; a 'vanilla' step
+       :concurrency 2 ;; run all steps with 2 threads (unless overridden)
+       :error-handler (fn [ex m] ;; do something special with errors, and retry
+                        ...
+                        (pl/*pipeline* m :step *current-step*))))
+ 
+   ;; put data onto the front pipeline
+   (foo-pipeline {:bar 1 :ham \"biscuit\"})
+ 
+   ;; put data onto the pipeline at a given step
+   (foo-pipeline {:bar 1 :foo 42 :ham \"gravy\"} :step :update-bar)
+ 
+   ;; optional - it will automatically be stopped on undeploy
+   (pl/stop foo-pipeline)
+ 
+   This API is alpha, and subject to change."
   (:use [immutant.util :only (mapply app-name)])
   (:require [clojure.tools.logging :as log]
             [immutant.messaging    :as msg]))
@@ -40,9 +88,14 @@
    handler function."
   ::halt)
 
-(def ^:private pipelines (atom #{}))
+(def ^:private pipelines
+  "Stores the currently active pipelines"
+  (atom #{}))
 
-(defn- wrap-error-handler [f {:keys [error-handler]}]
+(defn- wrap-error-handler
+  "Wraps the given function in an error handler if one is specified in
+  the options."
+  [f {:keys [error-handler]}]
   (if error-handler
     (fn [m]
       (try
@@ -51,12 +104,15 @@
           (error-handler e m))))
     f))
 
-(defn- wrap-step-bindings [f current next]
+(defn- wrap-step-bindings
+  [f current next]
   (binding [*current-step* current
             *next-step* next]
     (bound-fn* f)))
 
-(defn- pipeline-listen [pl opts f]
+(defn- pipeline-listen
+  "Creates a listener on the pipeline for the given function."
+  [pl opts f]
   (let [{:keys [step next-step]} (meta f)
         opts (-> opts
                  (merge (meta f))
@@ -75,14 +131,19 @@
               wrapped-f)            
             opts)))
 
-(defn- pipeline-fn [pl entry]
+(defn- pipeline-fn
+  "Creates a fn that places it's first arg onto the pipeline,
+  optionally at a step specified by :step"
+  [pl entry]
   (vary-meta
    (fn [m & {:keys [step] :or {step entry}}]
      (msg/publish pl m :properties {"step" step}))
    assoc
    :pipeline pl))
 
-(defn- named-steps [fns]
+(defn- named-steps
+  "Associates step names with each step function via metadata."
+  [fns]
   (let [step-names (-> (map-indexed (fn [n s]
                                       (str (or (:name (meta s)) n))) fns)
                        vec
@@ -93,8 +154,35 @@
      fns (partition 2 1 step-names))))
 
 (defn pipeline
-  "Creates a pipeline function. A pipeline is a sequence of chained
-  functions (\"steps\"), each receiving the result of the previous step."
+  "Creates a pipeline function.
+
+   It takes a unique (within the scope of the application) name, one
+   or more single-arity functions, and optional kwarg options, and
+   returns a function that places its argument onto the pipeline when
+   called.
+
+   The following kwarg options are supported, and must follow the step
+   functions [default]:
+
+   :error-handler  a function that will be called when any step raises
+                   an exception. It will be passed the exception and
+                   the argument to the step. Without an error-handler,
+                   the default HornetQ retry semantics will be
+                   used. Can be overridden on a per-step basis - see
+                   the 'step' function. [nil]
+   :concurrency    the number of threads to use for *each* step. Can be
+                   overridden on a per-step basis - see the 'step'
+                   function. [1]
+
+   During the execution of each step and each error-handler call, the
+   following vars are bound:
+
+   *pipeline*      the pipeline (as a fn) that is being executed
+   *current-step*  the name of the currently executing step
+   *next-step*     the name of the next step in the pipeline
+
+   This function is *not* idempotent. Attempting to create a pipeline
+   with the same name as an existing pipeline will raise an error."
   [name & args]
   (let [steps (named-steps (take-while fn? args))
         opts (apply hash-map (drop-while fn? args)) 
@@ -112,12 +200,23 @@
         (vary-meta pl-fn assoc :listeners listeners)))))
 
 (defn step
-  "Creates a step with additional options. {{what options?}}"
+  "Wraps the given function with the given options, returning a function.
+
+   The following options are supported [default]:
+
+   :name           a name to use for the step [the current index of the fn]
+   :concurrency    the number of threads to use, overriding the pipeline
+                   setting. See the docs for 'pipeline' [1]
+   :error-handler  an error handler function that can override the
+                   pipeline setting. See the docs for 'pipeline' [nil]"
   [f & {:as opts}]
   (vary-meta f merge opts))
 
 (defn stop
-  "Shuts down the given pipeline."
+  "Destroys a pipeline. Typically not necessary since it will be done
+   for you when your app is undeployed. This will fail with a warning
+   if any messages are yet to be delivered unless ':force true' is
+   passed. Returns true on success."
   [pl & args]
   (let [{:keys [pipeline listeners]} (meta pl)]
     (swap! pipelines disj pipeline)
