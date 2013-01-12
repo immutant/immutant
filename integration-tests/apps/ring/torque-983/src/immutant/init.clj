3315471282e6c3d7a7104be11fd8697de2c47c2f
Jim Crossley <jim@crossleys.org>
2013-01-12 11:32:37 -0500

    The TORQUE-983 bug is real, so renaming queue to avoid test conflicts

--- integration-tests/apps/ring/torque-983/src/immutant/init.clj
+++ integration-tests/apps/ring/torque-983/src/immutant/init.clj
@@ -2,16 +2,16 @@
   (:require [immutant.web :as web]
             [immutant.messaging :as msg]))
 
-(msg/start "/queue/test")
+(msg/start "/queue/torque-983")
 (msg/start "/queue/results")
 
 (defn msg-handler [msg]
-  (msg/publish "/queue/results" (conj msg :msg) :priority 1))
+  (msg/publish "/queue/results" (conj msg :msg)))
 
-(msg/listen "/queue/test" msg-handler)
+(msg/listen "/queue/torque-983" msg-handler)
 
 (defn web-handler [request]
-  (msg/publish "/queue/test" [:web] :priority 8)
+  (msg/publish "/queue/torque-983" [:web] :priority 8)
   {:status 200
    :body "nothing"})
 
