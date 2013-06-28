8a1e5f1d12976074afafaded98bb736c61ee6486
Toby Crawley <toby@tcrawley.org>
2013-06-28 07:43:39 -0400

    Pin to a clojure version for smoketest.
    lein 2.2.0 now injects nrepl as a userspace dependency, and without an
    explicit clojure dep, it brings in 1.2.0, which we don't support (it
    doesn't have IBlockingDeref).

--- support/smoketest/project.clj
+++ support/smoketest/project.clj
@@ -1,3 +1,4 @@
 (defproject smoketest "0.1.0-SNAPSHOT"
-  :dependencies [[org.immutant/immutant ~(System/getProperty "version")]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.immutant/immutant ~(System/getProperty "version")]]
   :local-repo "target/.m2")
