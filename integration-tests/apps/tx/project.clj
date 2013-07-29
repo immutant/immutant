b7cc89b0b6179864d415b48eb1f436df7339bc0e
Jim Crossley <jim@crossleys.org>
2013-07-29 16:11:34 -0400

    Update pg driver

--- integration-tests/apps/tx/project.clj
+++ integration-tests/apps/tx/project.clj
@@ -6,6 +6,6 @@
                  [com.h2database/h2 "1.3.160"]
                  [org.clojars.gukjoon/ojdbc "1.4"]
                  [mysql/mysql-connector-java "5.1.22"]
-                 [postgresql "9.0-801.jdbc4"]
+                 [postgresql "9.1-901.jdbc4"]
                  [net.sourceforge.jtds/jtds "1.2.4"]]
   :immutant {:swank-port 4005})
