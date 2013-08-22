1639547aa0ce5203f43af1b598983d7794cac774
Jim Crossley <jim@crossleys.org>
2013-08-22 18:33:33 -0400

    Covered in immutant.xa/wrap

--- modules/xa/src/main/clojure/immutant/xa/wrappers.clj
+++ modules/xa/src/main/clojure/immutant/xa/wrappers.clj
@@ -15,7 +15,7 @@
 ;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 ;; 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-(ns immutant.xa.wrappers
+(ns ^{:no-doc true} immutant.xa.wrappers
   "Provide XA-compliant wrappers for java.sql interfaces"
   (:import [java.sql PreparedStatement Connection])
   (:require [immutant.xa.transaction :as tx]))
