6baeba84de391fbe73f95d296cdd2493847c3903
Jim Crossley <jim@crossleys.org>
2013-08-22 18:30:54 -0400

    Not ready to doc this just yet

--- modules/cache/src/main/clojure/immutant/cache/config.clj
+++ modules/cache/src/main/clojure/immutant/cache/config.clj
@@ -15,7 +15,7 @@
 ;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 ;; 02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
-(ns immutant.cache.config
+(ns ^{:no-doc true} immutant.cache.config
   (:import [org.infinispan.configuration.cache VersioningScheme CacheMode]
            [org.infinispan.transaction TransactionMode LockingMode]
            org.infinispan.transaction.lookup.GenericTransactionManagerLookup
