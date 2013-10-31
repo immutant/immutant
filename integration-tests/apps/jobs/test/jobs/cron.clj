cc8654a0101fc0a4701c9d3719b885601794d309
Jim Crossley <jim@crossleys.org>
2013-10-31 18:02:44 -0400

    Cut out another 30s, though the steps may be aggressive for CI

--- integration-tests/apps/jobs/test/jobs/cron.clj
+++ integration-tests/apps/jobs/test/jobs/cron.clj
@@ -37,14 +37,14 @@
       (is (= ["ping" "ping"] (take 2 (msg/message-seq q1))))
       (with-job #(msg/publish q2 "pong")
         (is (= ["pong" "pong"] (take 2 (msg/message-seq q2))))
-        (is (not (msg/receive q1 :timeout 10000)))))))
+        (is (not (msg/receive q1 :timeout 2000)))))))
 
 (deftest unschedule
   (let [q (random-queue)]
     (with-job #(msg/publish q "ping")
       (is (msg/receive q :timeout 10000))
       (job/unschedule "a-job")
-      (is (not (msg/receive q :timeout 5000))))))
+      (is (not (msg/receive q :timeout 2000))))))
 
 (deftest job-should-have-correct-CL
   (let [q (random-queue)]
@@ -80,7 +80,7 @@
       (is (msg/receive q :timeout 10000))
       (require '[immutant.jobs :as job] :reload-all)
       (job/unschedule "a-job")
-      (Thread/sleep 5000)
+      (Thread/sleep 2000)
       (let [curval (.get aint)]
         (Thread/sleep 2000)
         (is (= curval (.get aint)))))))
