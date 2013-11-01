25742cfa910f8542cebfda85cff2580d5c4456c4
Jim Crossley <jim@crossleys.org>
2013-11-01 16:50:49 -0400

    Update pedestal version

--- integration-tests/apps/pedestal/hello/project.clj
+++ integration-tests/apps/pedestal/hello/project.clj
@@ -3,13 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [io.pedestal/pedestal.service "0.1.1"]
-
-                 ;; Remove this line and uncomment the next line to
-                 ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.1.1"]
-                 ;; [io.pedestal/pedestal.tomcat "0.1.1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [io.pedestal/pedestal.service "0.2.1"]
 
                  ;; Logging
                  [ch.qos.logback/logback-classic "1.0.7"]
