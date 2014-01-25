72f1ac1a04cecd652428f947ba5a2aaea6d6c6cf
Jim Crossley <jim@crossleys.org>
2014-01-25 11:19:32 -0500

    New plugin config

--- modules/project.clj
+++ modules/project.clj
@@ -2,7 +2,7 @@
   :description "Parent for all modules"
   ;; :parent [org.immutant/immutant-parent "1.0.3-SNAPSHOT"
   ;; :relative-path "../pom.xml"]
-  :profiles {:inherited {:dependencies [[org.clojure/clojure "1.5.1"]
+  :modules  {:inherited {:dependencies [[org.clojure/clojure "1.5.1"]
                                         [org.jboss.as/jboss-as-server "7.2.x.slim.incremental.12"]
                                         [midje "1.6.0" :scope "test"]
                                         [org.immutant/immutant-clojure-test-support "1.0.3-SNAPSHOT" :scope "test"]
