3ec5539d3191bacbae347980c30b8fc52c5818e8
Jim Crossley <jim@crossleys.org>
2014-02-01 13:02:18 -0500

    Missed these

--- support/build-support/project.clj
+++ support/build-support/project.clj
@@ -4,5 +4,8 @@
   :dependencies [[org.clojure/data.zip "0.1.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [commons-io "2.0.1"]
+                 [clj-glob "1.0.0"]
+                 [org.clojars.tcrawley/codox.core "0.6.3"]
+                 [digest "1.4.0"]
                  [cheshire _]
                  [leiningen-core _]])
