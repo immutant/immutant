cebd2638431d05b2a451645e52aecc50f609b926
Toby Crawley <toby@tcrawley.org>
2014-02-03 14:57:54 -0500

    Revert "Don't ship a pre-built logging.properties, since the file path has to be absolute."
    This reverts commit e534bdc92719a276e6cea2db0d414f68846b8aa3.

--- support/build-support/src/main/clojure/immutant/build/tools.clj
+++ support/build-support/src/main/clojure/immutant/build/tools.clj
@@ -551,6 +551,8 @@
     (with-message "Copying config files"
       (io/copy (io/file (format "src/resources/standalone.%s.xml" type))
         (io/file (jboss-dir) "standalone/configuration/standalone.xml"))
+      (io/copy (io/file (format "src/resources/logging-standalone.properties" type))
+               (io/file (jboss-dir) "standalone/configuration/logging.properties"))
       (io/copy (io/file (format "src/resources/standalone-ha.%s.xml" type))
                (io/file (jboss-dir) "standalone/configuration/standalone-ha.xml"))
       (io/copy (io/file (format "src/resources/domain.%s.xml" type))
