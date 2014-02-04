b9ac35181c8a45a3063393fd0b499a669c0b39b6
Jim Crossley <jim@crossleys.org>
2014-02-04 18:09:53 -0500

    A build plugin, currently with two hooks
    One ensures that pom.xml files are written to target/ rather than
    overwriting the old ones.
    
    The other obviates generate_clojars_project.clj for the most part.

---
+++ support/build-plugin/src/leiningen/immutant.clj
@@ -0,0 +1,25 @@
+(ns leiningen.immutant
+  (:require [robert.hooke]
+            [leiningen.pom]
+            [leiningen.jar]
+            [clojure.java.io :as io]))
+
+(defn copy-jar [f & args]
+  (let [[project main] args]
+    (if-let [src (and (= 2 (count args)) (:src-jar project))]
+      (let [src (io/file (:root project) (.replace src "${version}" (:version project)))
+            dest (io/file (:target-path project)
+                   (format "%s-%s.jar" (:name project) (:version project)))]
+        (.mkdirs (.getParentFile dest))
+        (io/copy src dest))
+      (apply f args))))
+
+(defn put-pom-in-target [f & args]
+  (let [[project pom] args]
+    (if (= pom "pom.xml")
+      (f project "target/pom.xml")
+      (apply f args))))
+
+(defn hooks []
+  (robert.hooke/add-hook #'leiningen.pom/pom #'put-pom-in-target)
+  (robert.hooke/add-hook #'leiningen.jar/jar #'copy-jar))
