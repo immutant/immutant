5acbd214f950f5ed6096fd30c56e8256e494c5f9
Jim Crossley <jim@crossleys.org>
2014-02-04 23:12:21 -0500

    These need to be AOT'd

--- support/build-support/project.clj
+++ support/build-support/project.clj
@@ -1,6 +1,7 @@
 (defproject org.immutant/immutant-build-support "1.0.3-SNAPSHOT"
   :parent [org.immutant/immutant-support-parent _ :relative-path "../pom.xml"]
-  :plugins [[lein-modules "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-modules "0.1.0-SNAPSHOT"]
+            [org.immutant/build-plugin "0.1.0-SNAPSHOT"]]
   :dependencies [[org.clojure/data.zip "0.1.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [commons-io "2.0.1"]
@@ -8,4 +9,5 @@
                  [org.clojars.tcrawley/codox.core "0.6.3"]
                  [digest "1.4.0"]
                  [cheshire _]
-                 [leiningen-core _]])
+                 [leiningen-core _]]
+  :aot [#"immutant.build"])
