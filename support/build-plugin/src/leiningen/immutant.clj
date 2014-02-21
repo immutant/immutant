8e10f8b9adb786cf917d6a8e6e2a1c4049c4a0e6
Jim Crossley <jim@crossleys.org>
2014-02-21 12:14:13 -0500

    Create the "module assemblies"

--- support/build-plugin/src/leiningen/immutant.clj
+++ support/build-plugin/src/leiningen/immutant.clj
@@ -3,10 +3,13 @@
             [leiningen.pom]
             [leiningen.jar]
             [leiningen.core.main :as main]
+            [leiningen.core.project :as prj]
+            [leiningen.core.classpath :as cp]
             [clojure.java.io :as io]))
 
 (defn copy-jar [f & args]
-  (let [[project main] args]
+  "Only because our 'namespaces' jars are really our 'modules' jars"
+  (let [[project] args]
     (if-let [src (and (= 2 (count args)) (:src-jar project))]
       (let [src (io/file (:root project)
                   (.replace src "${version}" (:version project)))
@@ -24,6 +27,26 @@
       (f project "target/pom.xml")
       (apply f args))))
 
+(defn- copy-sans-version [dir file]
+  (let [name (apply str (rest (re-find #"(.*)-\d.*(\..*)$" (.getName file))))
+        dest (io/file dir (if (empty? name) (.getName file) name))]
+    (io/copy file dest)))
+
+(defn prepare-module-after-jar [f & args]
+  (let [result (apply f args)
+        [project] args
+        module (io/file (:root project) "src/module/resources/module.xml")]
+    (if (.exists module)
+      (let [dir (io/file (:target-path project) (str (:name project) "-module"))]
+        (.mkdirs dir)
+        (spit (io/file dir "module.xml")
+          (.replace (slurp module) "${project.artifactId}" (:name project)))
+        (copy-sans-version dir (io/file (result [:extension "jar"])))
+        (doseq [dep (cp/resolve-dependencies :dependencies (prj/unmerge-profiles project [:default]))]
+          (copy-sans-version dir dep))))
+    result))
+
 (defn hooks []
   (robert.hooke/add-hook #'leiningen.pom/pom #'put-pom-in-target)
-  (robert.hooke/add-hook #'leiningen.jar/jar #'copy-jar))
+  (robert.hooke/add-hook #'leiningen.jar/jar #'copy-jar)
+  (robert.hooke/add-hook #'leiningen.jar/jar #'prepare-module-after-jar))
