3d35ce598a16916e3746de9e320c6c62a62a901a
Jim Crossley <jim@crossleys.org>
2014-03-11 14:16:54 -0400

    Test for unmount, init and destroy

--- modules/web/test/immutant/web_test.clj
+++ modules/web/test/immutant/web_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [immutant.web :refer :all]
             [testing.web  :refer [get-body hello handler]]
-            [testing.hello.service :as pedestal]))
+            [testing.hello.service :as pedestal])
+  (import clojure.lang.ExceptionInfo))
 
 (def url "http://localhost:8080/")
 
@@ -27,3 +28,22 @@
 (deftest mount-pedestal-service
   (mount-servlet (server) pedestal/servlet)
   (is (= "Hello World!" (get-body url))))
+
+(deftest unmount-yields-404
+  (run hello)
+  (is (= "hello" (get-body url)))
+  (unmount)
+  (let [result (is (thrown? ExceptionInfo (get-body url)))]
+    (is (= 404 (-> result ex-data :object :status)))))
+
+(deftest mount-should-call-init
+  (let [p (promise)]
+    (run hello :init #(deliver p 'init))
+    (is (= 'init @p))))
+
+(deftest unmount-should-call-destroy
+  (let [p (promise)]
+    (run hello :destroy #(deliver p 'destroy))
+    (is (not (realized? p)))
+    (unmount)
+    (is (= 'destroy @p))))
