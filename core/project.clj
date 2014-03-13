1e8b113b691896b0a258eee0fb383cf92390393a
Jim Crossley <jim@crossleys.org>
2014-03-13 16:44:07 -0400

    Eliminated the modules/ directory
    All supporting subprojects should reside beneath etc/

--- core/project.clj
+++ core/project.clj
@@ -1,5 +1,5 @@
 (defproject org.immutant/core "2.0.0-SNAPSHOT"
-  :parent [org.immutant/immutant-parent _ :relative-path "../../project.clj"]
+  :parent [org.immutant/immutant-parent _ :relative-path "../project.clj"]
   :plugins [[lein-modules "0.2.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
