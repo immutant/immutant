59cd225721d765babb6507110b35f7455f1801fb
Toby Crawley <toby@tcrawley.org>
2014-04-01 21:34:09 -0400

    Back to another auth scheme.

--- project.clj
+++ project.clj
@@ -20,9 +20,7 @@
   :profiles {:provided {:dependencies [[org.clojure/clojure _]]}
              :fast {:modules {:subprocess false}}
              :incremental {:deploy-repositories [["release"
-                                                  {:url "dav:https://repository-projectodd.forge.cloudbees.com/incremental"
-                                                   :username :env/dav_user
-                                                   :password :env/dav_password}]]
+                                                  "dav:https://repository-projectodd.forge.cloudbees.com/incremental"]]
                            :plugins [[lein-webdav "0.1.0"]]}}
   
   :modules  {:inherited {:repositories [["projectodd-upstream"
