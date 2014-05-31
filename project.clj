b042430866b6d3a10492cd85aa4ea94c440b3e6a
Jim Crossley <jim@crossleys.org>
2014-05-31 00:20:07 -0400

    Take advantage of the :version feature
    Removes redundant references to the defproject version value

--- project.clj
+++ project.clj
@@ -63,7 +63,7 @@
                         tools.reader               "0.8.4"
                         ring                       "1.2.2"
 
-                        org.immutant               "2.0.0-SNAPSHOT"
                         org.projectodd.wunderboss  "1.x.incremental.42"
                         ;;org.projectodd.wunderboss  "0.1.0-SNAPSHOT"
-                        }})
+
+                        org.immutant               :version}})
