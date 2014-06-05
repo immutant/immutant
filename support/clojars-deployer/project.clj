b54aee4803a83da37b57e7428fc82c733825b29b
Toby Crawley <toby@tcrawley.org>
2014-06-05 15:05:28 -0400

    Update clojars-deployer to use latest leiningen.
    We were relying on a pomegranate snapshot that no longer existed.

--- support/clojars-deployer/project.clj
+++ support/clojars-deployer/project.clj
@@ -1,15 +1,15 @@
 ;; Copyright 2008-2014 Red Hat, Inc, and individual contributors.
-;; 
+;;
 ;; This is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU Lesser General Public License as
 ;; published by the Free Software Foundation; either version 2.1 of
 ;; the License, or (at your option) any later version.
-;; 
+;;
 ;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; Lesser General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU Lesser General Public
 ;; License along with this software; if not, write to the Free
 ;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
@@ -21,6 +21,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [leiningen "2.1.1"]
-                 [com.cemerick/pomegranate "0.2.1-SNAPSHOT"]]
+                 [leiningen "2.3.4"]]
   :main clojars-deployer.main)
