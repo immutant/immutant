fe6954774f22decaa1ce8946b7cf2c8b59c14874
Toby Crawley <toby@tcrawley.org>
2014-07-12 17:14:33 -0600

    Update jdbc integs to have clojure versions.
    Without a clojure dependency in the project.clj, the apps' clojure
    versions don't get updated by the fixture, and were all using clojure
    1.3.0 brought in transitively by java.jdbc.

--- integration-tests/apps/jdbc/0.2.x/project.clj
+++ integration-tests/apps/jdbc/0.2.x/project.clj
@@ -1,15 +1,15 @@
 ;; Copyright 2008-2014 Red Hat, Inc, and individual contributors.
-;; 
+;;
 ;; This is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU Lesser General Public License as
 ;; published by the Free Software Foundation; either version 2.1 of
 ;; the License, or (at your option) any later version.
-;; 
+;;
 ;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 ;; Lesser General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU Lesser General Public
 ;; License along with this software; if not, write to the Free
 ;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
@@ -18,4 +18,5 @@
 (defproject xa "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/java.jdbc "0.2.1"]
+                 [org.clojure/clojure "1.4.0"]
                  [com.h2database/h2 "1.3.160"]])
