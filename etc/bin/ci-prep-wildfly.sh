0fa695d837314fc022781c78a48efa896d3e1cc9
Toby Crawley <toby@tcrawley.org>
2014-08-19 11:22:37 -0400

    Update ci script to add test user.

--- etc/bin/ci-prep-wildfly.sh
+++ etc/bin/ci-prep-wildfly.sh
@@ -1,19 +1,27 @@
+#! /bin/sh
+
 dest_dir=$1
 version=$2
 jboss_home="${dest_dir}/wildfly-${version}"
 
+if [ -z "${version}" ]; then
+  echo "Usage: ${0} dest-dir wildfly-version"
+  exit 1
+fi
+
 if [ ! -d ${jboss_home} ]; then
+  echo "Installing WildFly ${version} to ${dest_dir}"
   cd ${dest_dir}
   wget http://download.jboss.org/wildfly/${version}/wildfly-${version}.tar.gz
   tar xf wildfly-${version}.tar.gz
   cd -
 fi
 
+# TODO: domain mode
 conf="${jboss_home}/standalone/configuration/standalone-full.xml"
-if [ $(grep -c NIO  ${conf}) -eq 0 ]; then
+if [ $(grep -c NIO ${conf}) -eq 0 ]; then
   echo "Enabling NIO journal"
   sed -i.bak "s/<hornetq-server>/<hornetq-server><journal-type>NIO<\/journal-type>/" ${conf}
-  # TODO: domain mode ^
   echo "Adding application user testuser:testuser"
   ${jboss_home}/bin/add-user.sh -a -u 'testuser' -p 'testuser' -g 'guest'
 fi
--- integration-tests/README.md
+++ integration-tests/README.md
@@ -7,5 +7,9 @@ You'll need to set the path to your WildFly install, either by setting
 
 to your :user profile in `~/.lein/profiles.clj`.
 
-Then, you can run all of the tests with `lein modules all`, or an
-individual test with `lein all` within the test project itself.
+You'll also need to ensure your WildFly install has a test user
+setup. The easiest way to do that is by using the ci prep script:
+
+    ../etc/bin/ci-prep-wildfly.sh path/to/wildfly/parent/dir 8.1.0.Final
+
+Then, you can run all of the tests with `lein with-profile +integs  all`.
