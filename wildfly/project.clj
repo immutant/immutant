db8d89a5709c3f63fdc06e1bc84cce71c375a6cb
Jim Crossley <jim@crossleys.org>
2014-08-26 10:59:07 -0400

    Version 1.9.0-SNAPSHOT

--- wildfly/project.clj
+++ wildfly/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/wildfly "1.9.0-SNAPSHOT"
+(defproject org.immutant/wildfly "1.9.0"
   :plugins [[lein-modules "0.3.8"]]
 
   :dependencies [[org.immutant/core _]
