947438350c7e2c5a54b4b994cad7cda230e41f5b
Jim Crossley <jim@crossleys.org>
2014-08-26 10:49:44 -0400

    Bogus version to test release task

--- core/project.clj
+++ core/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/core "2.0.0-SNAPSHOT"
+(defproject org.immutant/core "1.9.0-SNAPSHOT"
   :plugins [[lein-modules "0.3.8"]]
 
   :dependencies [[org.clojure/java.classpath _]
