f3d78aa532fca335dbe2450d962d0e2e8d05572a
Jim Crossley <jim@crossleys.org>
2014-08-26 12:57:36 -0400

    Version 2.0.0-SNAPSHOT

--- messaging/project.clj
+++ messaging/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/messaging "2.0.0-alpha1"
+(defproject org.immutant/messaging "2.0.0-SNAPSHOT"
   :plugins [[lein-modules "0.3.8"]]
 
   :dependencies [[org.immutant/core _]
