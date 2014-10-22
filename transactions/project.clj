93f65f70b26e356febb8c6459851e20652ca8ce8
Jim Crossley <jim@crossleys.org>
2014-10-22 17:16:30 -0400

    Version 2.0.0-alpha2

--- transactions/project.clj
+++ transactions/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/transactions "2.0.0-SNAPSHOT"
+(defproject org.immutant/transactions "2.0.0-alpha2"
   :plugins [[lein-modules "0.3.9"]]
 
   :dependencies [[org.immutant/core _]
