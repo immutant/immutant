8c18a67e13c4eb119cc67557bb60df018ed95363
Toby Crawley <toby@tcrawley.org>
2014-11-12 11:30:36 -0500

    Improve build output.

--- etc/bin/bees-ci-build.sh
+++ etc/bin/bees-ci-build.sh
@@ -9,47 +9,59 @@ BIN_DIR="${WORKSPACE}/bin"
 WF_DIR="${WORKSPACE}/wildfly-dists"
 WF8_VERSION="8.1.0.Final"
 WF9_VERSION="9.0.0.Alpha1"
+LEIN_VERSION=2.4.3
 export PATH="${BIN_DIR}:${PATH}"
 export WORKSPACE_HOME="${WORKSPACE}/home"
 export LEIN_HOME="${WORKSPACE_HOME}/.lein"
 export JVM_OPTS="-Dprogress.monitor=false"
 
+function mark {
+    echo
+    echo "=============================================="
+    echo $1
+    date
+    echo "=============================================="
+    echo
+}
+
 rm -rf ${WORKSPACE}/target ${BIN_DIR}
 
+mark "Installing leiningen ${LEIN_VERSION}"
 mkdir -p ${BIN_DIR}
 cd ${BIN_DIR}
-wget --no-check-certificate https://raw.github.com/technomancy/leiningen/2.4.3/bin/lein
+wget --no-check-certificate https://raw.github.com/technomancy/leiningen/${LEIN_VERSION}/bin/lein
 chmod +x lein
 cd -
 
+mark "Setting up lein profiles"
 mkdir -p ${LEIN_HOME}
 cp -f /private/projectodd/auth_profile.clj ${LEIN_HOME}/profiles.clj
 
 rm -rf ${WF_DIR}
 
+mark "Installing WildFly ${WF8_VERSION}"
 etc/bin/ci-prep-wildfly.sh ${WF_DIR} ${WF8_VERSION}
+
+mark "Installing WildFly ${WF9_VERSION}"
 etc/bin/ci-prep-wildfly.sh ${WF_DIR} ${WF9_VERSION}
 
+mark "Reversioning"
 etc/bin/reversion.sh 2.x.incremental.${BUILD_NUMBER}
 
-echo "Starting build + integ run against ${WF8_VERSION}"
+mark "Starting build + integ run against ${WF8_VERSION}"
 export JBOSS_HOME="${WF_DIR}/wildfly-${WF8_VERSION}"
-date
 lein with-profile +integs modules all
 
-echo "Starting integs with ${WF9_VERSION}"
+mark "Starting integs with ${WF9_VERSION}"
 export JBOSS_HOME="${WF_DIR}/wildfly-${WF9_VERSION}"
-date
 cd integration-tests
 lein with-profile +integs all
 cd -
 
-echo "Starting deploy build"
-date
+mark "Starting deploy build"
 lein with-profile +incremental modules deploy
 
-echo "Starting doc build"
-date
+mark "Starting doc build"
 lein docs
 
-date
+mark "Done"
