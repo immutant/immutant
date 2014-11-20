13901e2a9eb12cd1168c9b0acdfbc8a34ac1d100
Toby Crawley <toby@tcrawley.org>
2014-11-20 13:09:21 -0500

    Version 2.0.0-beta1

--- transactions/project.clj
+++ transactions/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/transactions "2.0.0-SNAPSHOT"
+(defproject org.immutant/transactions "2.0.0-beta1"
   :description "Provides support for distributed (XA) transactions."
 
   :plugins [[lein-modules "0.3.9"]]
