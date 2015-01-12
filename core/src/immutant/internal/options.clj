5da81c02df5ce15b37ddfee4585095434f2e23e5
Toby Crawley <toby@tcrawley.org>
2015-01-12 12:20:41 -0500

    Fix class init issue under clojure 1.7.0.

--- core/src/immutant/internal/options.clj
+++ core/src/immutant/internal/options.clj
@@ -69,6 +69,9 @@
 (defn ^java.util.Map opts->map
   "Converts an Option class into a map of name -> Option instance."
   [class]
+  ;; clojure 1.7.0 no longer initializes classes on import, so we have
+  ;; to force init here (see CLJ-1315)
+  (Class/forName (.getName class))
   (->> class
     Option/optsFor
     (map (fn [^Option o] (vector (.name o) o)))
