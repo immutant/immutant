1f8cbd7d50446424a52db732d7e6c8a60ebb558b
Toby Crawley <toby@tcrawley.org>
2015-01-14 14:36:13 -0500

    Update copyrights for 2015.

--- etc/build-helper/src/build_helper/docs/util.clj
+++ etc/build-helper/src/build_helper/docs/util.clj
@@ -1,4 +1,4 @@
-;; Copyright 2014 Red Hat, Inc, and individual contributors.
+;; Copyright 2014-2015 Red Hat, Inc, and individual contributors.
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
