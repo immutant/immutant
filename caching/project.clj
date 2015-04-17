4015f59303cb8ed95f0940d2208331c31ea31244
Toby Crawley <toby@tcrawley.org>
2015-04-17 09:40:44 -0400

    Version 2.1.0-SNAPSHOT

--- caching/project.clj
+++ caching/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/caching "2.0.0"
+(defproject org.immutant/caching "2.1.0-SNAPSHOT"
   :description "Create, manage and manipulate Infinispan caches."
   :pedantic? false
   :plugins [[lein-modules "0.3.11"]]
