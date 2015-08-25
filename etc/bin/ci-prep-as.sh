dfe789eef8d0c9397b4ea06ee4c8f76942e60e0b
Jim Crossley <jim@crossleys.org>
2015-08-25 17:03:03 -0400

    Make this more useful

--- etc/bin/ci-prep-as.sh
+++ etc/bin/ci-prep-as.sh
@@ -26,21 +26,21 @@ if [ ! -d ${jboss_home} ]; then
         tar xf ${EAP_ARCHIVE_DIR}/EAP-${version}.tar.gz
     fi
     cd -
+fi
 
-    conf="${jboss_home}/standalone/configuration/standalone-full.xml"
-    if [ $(grep -c NIO ${conf}) -eq 0 ]; then
-        echo "Enabling NIO journal to avoid AIO failures"
-        perl -p -i -e "s:(<hornetq-server>)$:\1<journal-type>NIO</journal-type>:" $(ls ${jboss_home}/*/configuration/*)
-    fi
+conf="${jboss_home}/standalone/configuration/standalone-full.xml"
+if [ $(grep -c NIO ${conf}) -eq 0 ]; then
+    echo "Enabling NIO journal to avoid AIO failures"
+    perl -p -i -e "s:(<hornetq-server>)$:\1<journal-type>NIO</journal-type>:" $(ls ${jboss_home}/*/configuration/*)
+fi
 
-    #echo "Enabling TRACE logging"
-    #sed -i.bak '/<root-logger>/{N; s/<root-logger>.*<level name="INFO"/<root-logger><level name="TRACE"/g}' ${conf}
-    echo "Adding application user testuser:testuser1!"
-    ${jboss_home}/bin/add-user.sh --silent -a -u 'testuser' -p 'testuser1!' -g 'guest'
+#echo "Enabling TRACE logging"
+#sed -i.bak '/<root-logger>/{N; s/<root-logger>.*<level name="INFO"/<root-logger><level name="TRACE"/g}' ${conf}
+echo "Adding application user testuser:testuser1!"
+${jboss_home}/bin/add-user.sh --silent -a -u 'testuser' -p 'testuser1!' -g 'guest'
 
-    # see https://github.com/jboss-developer/jboss-eap-quickstarts/tree/6.4.x/websocket-hello#review-the-modified-server-configuration
-    if [ "$type" = "EAP" ]; then
-        echo "Setting EAP connector type for websockets"
-        perl -p -i -e "s:HTTP/1.1:org.apache.coyote.http11.Http11NioProtocol:" $(ls ${jboss_home}/*/configuration/*)
-    fi
+# see https://github.com/jboss-developer/jboss-eap-quickstarts/tree/6.4.x/websocket-hello#review-the-modified-server-configuration
+if [ "$type" = "EAP" ]; then
+    echo "Setting EAP connector type for websockets"
+    perl -p -i -e "s:HTTP/1.1:org.apache.coyote.http11.Http11NioProtocol:" $(ls ${jboss_home}/*/configuration/*)
 fi
