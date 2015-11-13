bd7545db85415d566f360b69cdb73ffc50761333
Toby Crawley <toby@tcrawley.org>
2015-11-13 08:18:19 -0500

    Version 2.1.2-SNAPSHOT

--- integration-tests/project.clj
+++ integration-tests/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/integs "2.1.1"
+(defproject org.immutant/integs "2.1.2-SNAPSHOT"
   :pedantic? false
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[org.immutant/immutant _]
