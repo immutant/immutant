d8ab48e865cd2d5c8fc4e4c8036603de717cd725
Toby Crawley <toby@tcrawley.org>
2016-04-20 08:55:23 -0400

    Version 2.1.5-SNAPSHOT

--- web/project.clj
+++ web/project.clj
@@ -12,7 +12,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject org.immutant/web "2.1.4"
+(defproject org.immutant/web "2.1.5-SNAPSHOT"
   :description "Serve web requests using Ring handlers, Servlets, or Undertow HttpHandlers."
   :pedantic? false
   :plugins [[lein-modules "0.3.11"]]
